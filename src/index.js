bf0dca8de44a2182e5cec05ab856125d571ba858
dat lu <Ludat2410@gmail.com>
2025-01-30 17:44:16 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // template engine
 app.engine('hbs', exphbs.engine({
@@ -28,6 +33,11 @@ app.get('/news', (req, res) => {
 
 app.get('/search', (req, res) => {
   res.render('search');
+});
+
+app.post('/search',(req,res)=>{
+  console.log(req.body);
+  res.send('');
 });
 
 app.listen(port, () => {
